9770253b09907329baf815907d11afb764017ee8
adrisanchu <adrian.sanchez@constant-urban.eu>
2026-03-31 23:24:23 +0200

    added dialog component

---
+++ src/lib/components/ui/dialog/index.ts
@@ -0,0 +1,31 @@
+import Root from "./dialog.svelte";
+import Content from "./dialog-content.svelte";
+import Header from "./dialog-header.svelte";
+import Footer from "./dialog-footer.svelte";
+import Title from "./dialog-title.svelte";
+import Description from "./dialog-description.svelte";
+import Overlay from "./dialog-overlay.svelte";
+import Portal from "./dialog-portal.svelte";
+import Close from "./dialog-close.svelte";
+
+export {
+	Root,
+	Content,
+	Header,
+	Footer,
+	Title,
+	Description,
+	Overlay,
+	Portal,
+	Close,
+	//
+	Root as Dialog,
+	Content as DialogContent,
+	Header as DialogHeader,
+	Footer as DialogFooter,
+	Title as DialogTitle,
+	Description as DialogDescription,
+	Overlay as DialogOverlay,
+	Portal as DialogPortal,
+	Close as DialogClose,
+};
